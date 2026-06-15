f1759e9d8224cdc41dbb3cb4eb6cc1794ceba45b
aj-yost <aj.yost4@gmail.com>
2026-06-15 13:36:06 -0400

    Adding old 2026 shows

--- shows.js
+++ shows.js
@@ -52,4 +52,60 @@ start: "2026-09-06",
 location: "E. Water St, Lock Haven, PA",
 description: "Bear Bash Festival in Lock Haven"
 }
+
+{
+title: "Cafe 210 West",
+start: "2026-05-29T21:00:00",
+end: "2026-05-30T00:00:00",
+location: "210 W College Ave, State College, PA"
+},
+
+{
+title: "Boalsburg Memorial Day Festival",
+start: "2026-05-25",
+location: "Main Street, Boalsburg, PA",
+description: "Catch A Rock Show in downtown Boalsburg for the Memorial Day festival"
+}
+
+{
+title: "Smokehouse Bar & Grill",
+start: "2026-05-23",
+location: "15 Commerce Dr, Milroy, PA",
+},
+
+{
+title: "Cafe 210 West",
+start: "2026-05-07"
+location: "210 W College Ave, State College, PA"
+},
+
+{
+title: "Arena Bar & Grill",
+start: "2026-03-13"
+location: "1521 Martin St, State College, PA"
+},
+
+{
+title: "Cafe 210 West",
+start: "2026-02-28"
+location: "210 W College Ave, State College, PA"
+},
+
+{
+title: "The Bar",
+start: "2026-02-21"
+location: "306 Boal Ave, Boalsburg, PA"
+},
+
+{
+title: "Huntingdon Elks Lodge",
+start: "2026-01-30"
+location: "10431 Fairgrounds Rd, Huntingdon, PA"
+},
+
+{
+title: "Arena Bar & Grill",
+start: "2026-01-09"
+location: "1521 Martin St, State College, PA"
+},
 ];
